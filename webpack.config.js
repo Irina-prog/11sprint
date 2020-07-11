f20c42f332e1bd176f23bcb0c606fdb93ceb0b1e
Irina Belchikova <iboronenkova@gmail.com>
2020-07-11 10:39:34 +0300

    installed all plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash'); // добавили плагин
 
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+// подключаем плагин
+const isDev = process.env.NODE_ENV === 'development';
+// создаем переменную для development-сборки
+
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: './src/js/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
@@ -19,16 +24,42 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-  {
-        test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
-       }
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
+            'css-loader', 
+            'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          'file-loader?name=./src/images/[name].[ext]', // указали папку, куда складывать изображения
+          {
+            loader: 'image-webpack-loader',
+            options: {}
+          }
+        ]
+ }
     ]
   },
   plugins: [ 
     new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css'
+      filename: 'style.[contenthash].css'
     }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+              preset: ['default'],
+      },
+      canPrint: true
+    }), // подключите плагин после MiniCssExtractPlugin
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
